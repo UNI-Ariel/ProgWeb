b61939f60c1a0cba5ed304bf7a17ad6754691150
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2023-10-02 03:32:01 -0400

    Interfaz basica añadida.

---
+++ 02_webMovil/scripts.js
@@ -0,0 +1,8 @@
+function showMenu() {
+  document.getElementById('sideMenuBackground').style.display = "block";
+  document.getElementById('sideMenu').style.display = "block";
+}
+function hideMenu() {
+  document.getElementById('sideMenu').style.display = "none";
+  document.getElementById('sideMenuBackground').style.display = "none";
+}
