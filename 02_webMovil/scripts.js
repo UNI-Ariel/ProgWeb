ba1e4ed489919faab00f68d239fdfa839f515e0a
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2023-10-02 23:13:33 -0400

    Agregado items del menu.

--- 02_webMovil/scripts.js
+++ 02_webMovil/scripts.js
@@ -1,8 +1,46 @@
-function showMenu() {
-  document.getElementById('sideMenuBackground').style.display = "block";
-  document.getElementById('sideMenu').style.display = "block";
+var menu = document.getElementById("sideMenu");
+var menuO = document.getElementById("sideMenuBackground");
+
+var iframe = document.createElement("iframe");
+var iframeTarget = document.getElementById("frameTarget");
+
+iframe.width = "100%";
+iframe.height = "auto";
+iframe.style.margin = "0px";
+iframe.style.padding = "0px";
+iframe.style.border = "none";
+
+const paginas = [
+  {
+    type : 'consultas',
+    file : 'consultaAula.html'
+  },
+  {
+    type : 'reservas',
+    file : 'reservaAula.html'
+  }
+]
+
+function showNav() {
+  menu.style.width = "auto";
+  menuO.style.display = "block";
 }
-function hideMenu() {
-  document.getElementById('sideMenu').style.display = "none";
-  document.getElementById('sideMenuBackground').style.display = "none";
+
+function closeNav() {
+  menu.style.width = "0";
+  menuO.style.display = "none";
+}
+
+function formConsulta() {
+  iframe.src = paginas[0].file;
+  iframe.title = paginas[0].type;
+  iframeTarget.replaceWith(iframe);
+  closeNav();
+}
+
+function formReserva() {
+  iframe.src = paginas[1].file;
+  iframe.title = paginas[1].type;
+  iframeTarget.replaceWith(iframe);
+  closeNav();
 }
