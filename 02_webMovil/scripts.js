6956c7ec5e8cce3b18f0d95d0dd260af911b9d0f
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2023-10-03 23:28:02 -0400

    Unificado

--- 02_webMovil/scripts.js
+++ 02_webMovil/scripts.js
@@ -1,27 +1,5 @@
 var menu = document.getElementById("sideMenu");
 var menuO = document.getElementById("sideMenuBackground");
-var contenido = document.getElementById("content");
-
-var reserva = document.getElementById("reserva");
-var consulta = document.getElementById("consulta");
-
-reserva.addEventListener("click", function() {
-  mostrarContenido("reserva");
-});
-consulta.addEventListener("click",  function() {
-  mostrarContenido("consulta");
-});
-
-const paginas = [
-  {
-    type : 'consultas',
-    file : 'consultaAula.html'
-  },
-  {
-    type : 'reservas',
-    file : 'reservaAula.html'
-  }
-]
 
 function showNav() {
   menu.style.width = "200px";
@@ -31,15 +9,4 @@ function showNav() {
 function closeNav() {
   menu.style.width = "0";
   menuO.style.display = "none";
-}
-
-function mostrarContenido(tipo){
-  tipo = String(tipo);
-  if (tipo === "consulta"){
-    content.innerHTML = "<p>Usted ha seleccionado cosultas.</p>";
-  }
-  else{
-    content.innerHTML = "<p>Usted ha seleccionado reservas.</p>";
-  }
-  closeNav();
 }
