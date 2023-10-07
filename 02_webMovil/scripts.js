bcc1e3afa79b0c19f10a8bc92176248f45e4ca7f
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2023-10-07 01:35:34 -0400

    Cambio en la interfaz principal

--- 02_webMovil/scripts.js
+++ 02_webMovil/scripts.js
@@ -1,12 +1,3 @@
-var menu = document.getElementById("sideMenu");
-var menuO = document.getElementById("sideMenuBackground");
-
-function showNav() {
-  menu.style.width = "200px";
-  menuO.style.display = "block";
-}
-
-function closeNav() {
-  menu.style.width = "0";
-  menuO.style.display = "none";
+function setTime(){
+  document.getElementById('time').innerHTML = Date();
 }
