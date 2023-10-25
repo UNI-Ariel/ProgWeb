38e13354293e17d6b29f1ed5ad7985b8f74b4390
Ariel G. Valencia <ariel.valencia.0945@gmail.com>
2023-10-25 05:07:49 -0400

    Actualizar tablas

--- webMovil/src/db/tables.sql
+++ webMovil/src/db/tables.sql
@@ -1,40 +1,30 @@
 CREATE TABLE `ambiente` (
   `id_ambiente` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `id_estado` integer NOT NULL,
-  `id_tipo` integer NOT NULL,
-  `nombre` varchar(255) UNIQUE NOT NULL,
+  `id_tipo` varchar(64) NOT NULL,
+  `nombre` varchar(64) UNIQUE NOT NULL,
   `ubicacion` varchar(255) NOT NULL,
   `descripcion` varchar(255),
   `capacidad` integer NOT NULL,
-  `activo` boolean NOT NULL
-);
-
-CREATE TABLE `estado` (
-  `id_estado` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) UNIQUE NOT NULL,
-  `descripcion` varchar(255) NOT NULL
+  `deshabilitado` char(2) NOT NULL,
+  `activo` char(2) NOT NULL
 );
 
 CREATE TABLE `tipo` (
-  `id_tipo` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) UNIQUE NOT NULL,
+  `id_tipo` varchar(64) PRIMARY KEY NOT NULL,
   `descripcion` varchar(255) NOT NULL
 );
 
 CREATE TABLE `facilidad` (
-  `id_facilidad` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) UNIQUE NOT NULL,
+  `id_facilidad` varchar(64) PRIMARY KEY NOT NULL,
   `descripcion` varchar(255) NOT NULL
 );
 
 CREATE TABLE `facilidades` (
   `id_ambiente` integer NOT NULL,
-  `id_facilidad` integer NOT NULL,
+  `id_facilidad` varchar(64) NOT NULL,
   `detalles` varchar(255),
   PRIMARY KEY (`id_ambiente`, `id_facilidad`)
 );
-
-ALTER TABLE `ambiente` ADD FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`);
 
 ALTER TABLE `ambiente` ADD FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id_tipo`);
 
