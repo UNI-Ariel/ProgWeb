b6d8cc3f18ec7e8061057aa2997ed117bdbb3724
Gustavo Aguilar <aguilargustavo123321@gmail.com>
2023-11-08 00:58:20 -0400

    funcionCsvJSON

--- webMovil/src/public/js/scripts.js
+++ webMovil/src/public/js/scripts.js
@@ -47,7 +47,35 @@ function setTime(){
 function closeMessage() {
     document.getElementById('message').classList.toggle('hide');
 }
+/*
+function csvJSON(csv){
 
+    var lines=csv.split("\n");
+  
+    var result = [];
+  
+    var headers=lines[0].split(",");
+  
+    for(var i=1;i<lines.length;i++){
+  
+        var obj = {};
+        var currentline=lines[i].split(",");
+  
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+  
+        result.push(obj);
+  
+    }
+  
+    //return result; //JavaScript object
+    console.log(JSON.stringify(result));
+    return JSON.stringify(result); //JSON
+ }
+ document.querySelector('#archivocsv')
+  .addEventListener('change',csvJSON, false);
+*/
 /* function editAmbiente(event){
     const row = event.target.closest('tr');
     try{
